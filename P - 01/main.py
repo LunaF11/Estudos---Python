8be0864e030637659ed9cda7f5bd8cde555094ec
LunaF11 <Lunasantana120@gmail.com>
2025-03-12 18:04:51 -0300

    Conversão de tipos em Python

---
+++ P - 01/main.py
@@ -0,0 +1,10 @@
+nome = input ("digite seu nome: ")
+idade = input ("digite sua idade: ")
+peso = input ("digite seu peso: ")
+data_de_nascimento =2025-int(idade)
+
+
+print(nome)
+print(idade)
+print(float (peso))
+print(data_de_nascimento)
