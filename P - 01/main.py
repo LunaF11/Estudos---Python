e2e18189a6f30d1f23792a760b1c29b2e5f250dc
LunaF11 <Lunasantana120@gmail.com>
2025-03-13 17:39:09 -0300

    Operadores - P 02

--- P - 01/main.py
+++ P - 01/main.py
@@ -1,10 +1,21 @@
-nome = input ("digite seu nome: ")
-idade = input ("digite sua idade: ")
-peso = input ("digite seu peso: ")
-data_de_nascimento =2025-int(idade)
+# nome = input ("digite seu nome: ")
+# idade = input ("digite sua idade: ")
+# peso = input ("digite seu peso: ")
+# data_de_nascimento =2025-int(idade)
 
 
-print(nome)
-print(idade)
-print(float (peso))
-print(data_de_nascimento)
+# print(nome)
+# print(idade)
+# print(float (peso))
+# print(data_de_nascimento)
+
+soma = 1 + 1
+multiplicacao = 4 * 4
+divisao = 3 / 3
+potencia = 7 ** 2 
+
+print ("Soma:",soma)
+print ("Multiplicação:", multiplicacao)
+print ("Divisão:", divisao)
+print ("Potência:", potencia)
+
---
+++ P - 02/main.py
@@ -0,0 +1,9 @@
+soma = 1 + 1
+multiplicacao = 4 * 4
+divisao = 3 / 3
+potencia = 7 ** 2 
+
+print ("Soma:",soma)
+print ("Multiplicação:", multiplicacao)
+print ("Divisão:", divisao)
+print ("Potência:", potencia)
